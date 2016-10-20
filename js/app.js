7bdeb48c50a1afb59536b1ce1375aa59b1ef1ceb
Daniele <daniele2595@live.it>
2016-10-20 12:13:23 +0200

    correzione stampa tabella

--- js/app.js
+++ js/app.js
@@ -11,23 +11,21 @@ $(document).ready(function() {
        	url: "http://www.omdbapi.com/?s=" + titolo,
        	method: "GET",
        	success: function(response){
-       		console.log(response);
        		var i = 0;
-       		var n = response.totalResults;
+       		var n = response.Search.length;
        		if(response.Response == "False"){
        			alert(response.Error);
        		}
        		else{
+       			var tr = "";
 	       		for(i = 0; i < n ; i++){
-	       			// console.log(response.Search[i]); //ok
-	       			var tr = "";
-	       			tr = "<tr>" +
+	       			tr += "<tr>" +
 	       			"<td>" + response.Search[i].Title + "</td>" +
 	       			"<td>" + response.Search[i].Type + "</td>" +
 	       			"<td>" + response.Search[i].Year + "</td>" +
 	       			"</tr>";
-	       			$("#dati").html(tr);
 	       		}
+	       		$("#dati").html(tr);
 	       	}
        	},
        	error: function(response){
