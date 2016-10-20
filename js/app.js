71b36e9482db78967070c25c9792fbf5f9b7f9de
Daniele <daniele2595@live.it>
2016-10-20 12:02:08 +0200

    stampa messaggi di errore

--- js/app.js
+++ js/app.js
@@ -12,18 +12,23 @@ $(document).ready(function() {
        	method: "GET",
        	success: function(response){
        		console.log(response);
-       		var i=0;
-       		var n=response.totalResults;
-       		for(i=0;i<n;i++){
-       			// console.log(response.Search[i]); //ok
-       			var tr="";
-       			tr= "<tr>" +
-       			"<td>" + response.Search[i].Title + "</td>" +
-       			"<td>" + response.Search[i].Type + "</td>" +
-       			"<td>" + response.Search[i].Year + "</td>" +
-       			"</tr>";
-       			$("#dati").html(tr);
+       		var i = 0;
+       		var n = response.totalResults;
+       		if(response.Response == "False"){
+       			alert(response.Error);
        		}
+       		else{
+	       		for(i = 0; i < n ; i++){
+	       			// console.log(response.Search[i]); //ok
+	       			var tr = "";
+	       			tr = "<tr>" +
+	       			"<td>" + response.Search[i].Title + "</td>" +
+	       			"<td>" + response.Search[i].Type + "</td>" +
+	       			"<td>" + response.Search[i].Year + "</td>" +
+	       			"</tr>";
+	       			$("#dati").html(tr);
+	       		}
+	       	}
        	},
        	error: function(response){
        		console.log(response);
