07eadbddd29a347d7133d24a0c121a3754a594f0
Daniele <daniele2595@live.it>
2016-10-20 11:37:42 +0200

    aggiunta funzione in risposta alla chiamata ajax

--- js/app.js
+++ js/app.js
@@ -6,11 +6,22 @@ $(document).ready(function() {
 
    function cerca(){
        // cerca per titolo
-       var title = $("#title").text();
+       var titolo = $("#title").val();
        $.ajax({
-       	url: "http://www.omdbapi.com/?s=" + title,
+       	url: "http://www.omdbapi.com/?s=" + titolo,
        	method: "GET",
-
+       	success: function(response){
+       		console.log(response);
+       		var i=0;
+       		var n=response.totalResults;
+       		for(i=0;i<n;i++){
+       			// console.log(response.Search[i]); //ok
+       			
+       		}
+       	},
+       	error: function(response){
+       		console.log(response);
+       	}
        });
    };
 });
